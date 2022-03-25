6e6110cd2293f9ac3ea8a94480d8e583779e2719
aaliyanah56 <rahmaadannoor@gmail.com>
2022-03-25 02:09:39 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,46 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <string.h>
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ *infinite_add - Adds to numbers in a string
+ *@n1: The number
+ *@n2: The number
+ *@r: The result
+ *@size_r: The qty
+ *
+ *Return: none
  */
-
-void rev_string(char *n)
-{
-int i = 0;
-int j = 0;
-char temp;
-
-while (*(n + i) != '\0')
-{
-i++;
-}
-i--;
-
-for (j = 0; j < i; j++, i--)
-{
-temp = *(n + j);
-*(n + j) = *(n + i);
-*(n + i) = temp;
-}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
-
-while (*(n1 + i) != '\0')
-i++;
-while (*(n2 + j) != '\0')
-j++;
-i--;
-j--;
-if (j >= size_r || i >= size_r)
+char t;
+int  i, j, k, size_1, size_2, a, b, c =  0;
+size_1 = strlen(n1);
+size_2 = strlen(n2);
+if (size_1 > (size_r - 1) || size_2 > (size_r - 1))
 return (0);
-while (j >= 0 || i >= 0 || overflow == 1)
+for (i = --size_1, j = --size_2, k = 0; i >= 0 || j >= 0; k++)
 {
-if (i < 0)
-val1 = 0;
+if (i >= 0)
+a = n1[i--] - 48;
 else
-val1 = *(n1 + i) -'0';
-if (j < 0)
-val2 = 0;
+a = 0;
+if (j >= 0)
+b = n2[j--] - 48;
 else
-val2 = *(n2 + j) -'0';
-temp_tot = val1 + val2 + overflow;
-if (temp_tot >= 10)
-overflow = 1;
-else
-overflow = 0;
-if (digits >= (size_r - 1))
+b = 0;
+r[k] = (a + b + c) % 10 + 48;
+c = (a + b + c) / 10;
+if (k >= (size_r - 2))
 return (0);
-*(r + digits) = (temp_tot % 10) + '0';
-digits++
-j--;
-i--;
 }
-if (digits == size_r)
-return (0);
-*(r + digits) = '\0'
-rev_string(r);
+if (c > 0)
+r[k++] = c + 48;
+r[k] = '\0';
+for (i = 0; i < (k / 2); i++)
+{
+t = r[i];
+r[i] = r[k - i - 1];
+r[k - i - 1] = t;
+}
 return (r);
 }
