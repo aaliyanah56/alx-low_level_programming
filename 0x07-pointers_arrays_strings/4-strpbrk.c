85327d0983d54a3ff904417b2c8dd39865d86c32
aaliyanah56 <rahmaadannoor@gmail.com>
2022-03-28 10:58:00 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,29 +10,11 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s)
-{
-if (*s == c)
-return (s);
-s++;
-}
-return (NULL);
-}
-
-/**
- *_strpbrk - searches string for any of a set of bytes
- *@s: string to search
- *@accept: set of bytes to find
- *
- *Return: pointer to first matching byte or NULL
- */
-char *_strpbrk(char *s, char *accept)
-{
-while (*s)
-{
-if (_strchr(accept, *s))
-return (s);
-s++;
-}
-return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (!c ? s : NULL);
 }
