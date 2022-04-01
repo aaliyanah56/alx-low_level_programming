5c5ff64abb6de4e0a4c2200a31b88c084ea7739c
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-01 12:23:34 +0300

    .\0-whatsmyname.c

--- 0X0A-argc_argv/0-whatsmyname.c
+++ 0X0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+printf("%s\n", *argv);
 
-	return (0);
+return (0);
 }
--- 0X0A-argc_argv/main.h
+++ 0X0A-argc_argv/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H
-#define FILE MAIN
+#ifndef FILE_MAIN.H
+#define FILE_MAIN.H
 
 int _putchar(char c);
 int _atoi(char *s);
 
-#endi
+#endif
