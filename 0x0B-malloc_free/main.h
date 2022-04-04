2b672b2fe928410a3063cffc013f0bf8eadc1c2c
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-04 11:42:41 +0300

    .\0-create_array.c

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_HEADER_FILE
+#define Main-HEADER_FILE
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
+#endif
