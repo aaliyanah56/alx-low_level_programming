35a8cd3a5a07a1e935c86ecd7232ab215fb2be92
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-07 10:52:16 +0300

    .\0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_HEADER_FILE
+#define Main-HEADER_FILE
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
