0a550a7b390025754658f92def1f78d56e87d1ed
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-07 14:30:34 +0300

    .\2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,45 +1,43 @@
 #include "main.h"
 
 /**
-* *_memset - fills memory with a constant byte
-* @s: memory area to be filled
-* @b: char to copy
-* @n: number of times to copy b
-*
-* Return: pointer to the memory area s
+* *_memset - fills memory with a constant byte.
+* @s: pointer to put the constant
+* @b: constant
+* @n: max bytes to use
+* Return: s
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+char *ptr = s;
 
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
+while (n--)
+*s++ = b;
 
-return (s);
+return (ptr);
 }
 
 /**
-* *_calloc - allocates memory for an array
-* @nmemb: number of elements in the array
+* *_calloc - allocates memory for an array, using malloc
+* @nmemb: array length
 * @size: size of each element
-*
-* Return: pointer to allocated memory
+* Return: pointer
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr;
+void *m;
 
-if (nmemb == 0 || size == 0)
+if (size == 0 || nmemb == 0)
 return (NULL);
 
-ptr = malloc(size * nmemb);
+m = malloc(nmemb * size);
 
-if (ptr == NULL)
+if (m == 0)
 return (NULL);
 
-_memset(ptr, 0, nmemb * size);
+_memset(m, 0, nmemb * size);
 
-return (ptr);
+return (m);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_HEADER_FILE
 #define Main-HEADER_FILE
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
