0a20658a901b1045a80131ab6deb3050e4154c9b
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-13 20:24:28 +0300

    .\0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
 * print_name - prints a name.
 * @name: input name.
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
+
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
