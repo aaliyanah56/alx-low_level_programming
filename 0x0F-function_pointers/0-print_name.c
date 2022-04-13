30b0471b88085396d95c6199b74df14eb888c9a4
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-13 21:30:56 +0300

    .\0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,12 +6,11 @@
 *print_name - prints a name
 * @name: name to be printed
 * @f: pointer to function
-* Return: name
+* 
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
+if (name && f)
 f(name);
 }
