ac004d02100faeadd8e91f26ad0a3503d8a5b410
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-13 15:22:07 +0300

    .\0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
 * print_name - prints a name.
 * @name: input name.
@@ -11,6 +10,6 @@
 void print_name(char *name, void (*f)(char *))
 
 {
-if (name == NULL)
+if (f == NULL)
 f(name);
 }
