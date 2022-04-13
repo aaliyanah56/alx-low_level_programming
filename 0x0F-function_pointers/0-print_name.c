9af6466fa46bd3175e33a97f0c5e1277b58122d7
aaliyanah56 <rahmaadannoor@gmail.com>
2022-04-13 13:49:22 +0300

    .\0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name.
+* @name: input nameof a persom
+* @f: function pointer.
+*
+* Return: no return.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if(name == NULL)
+f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+function pointers learning in c
